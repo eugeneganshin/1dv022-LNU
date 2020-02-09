9c1fce3976208b7195fa9313ff71263e5022d1a6
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-02-09 21:00:46 +0300

    figured the loop

--- memory-game/src/js/app.js
+++ memory-game/src/js/app.js
@@ -1,5 +1,8 @@
 // import { MemoryGame } from './Game.js'
-import { Test } from './Test.js'
+// import { Test } from './Test.js'
+import { Lol } from './Lol.js'
 
 // const flip = new MemoryGame(3, 3)
 // const test = new Test(3, 3)
+const heh = new Lol(2, 2)
+document.querySelector('body').appendChild(heh)
