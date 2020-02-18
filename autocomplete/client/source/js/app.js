53c31519120ea1ddddc7160b08ecc6f061e1189f
Eugene Ganshin <e.s.ganshin.study@gmail.com>
2020-02-18 23:00:03 +0300

    Search bar with autocomplete feature v.1.0

--- autocomplete/client/source/js/app.js
+++ autocomplete/client/source/js/app.js
@@ -1,1 +1,21 @@
 import { TeamSelector } from './team-selector.js'
+
+const plteams = document.querySelector('#plteams')
+const template = document.querySelector('#cardTemplate')
+
+/**
+ * Simple function to create a template as we get data from the server
+ */
+plteams.addEventListener('teamselected', async event => {
+  const teamresult = await window.fetch(`${plteams.getAttribute('src')}/teams/${event.detail.id}`)
+  const teamDetail = await teamresult.json()
+
+  const teamCard = document.querySelector('.teamCard')
+  const card = template.content.cloneNode(true)
+  card.querySelector('b').textContent = teamDetail.name
+  card.querySelector('p').textContent = teamDetail.nickname
+  card.querySelector('a').setAttribute('href', teamDetail.url)
+  teamCard.innerHTML = ''
+  teamCard.appendChild(card)
+  console.log(teamDetail)
+})
--- autocomplete/client/source/js/team-selector.js
+++ autocomplete/client/source/js/team-selector.js
@@ -7,29 +7,50 @@ template.innerHTML = `
   <datalist id="teams"></datalist>
 </div>
 `
+/**
+ * A TeamSelector component
+ *
+ * @class TeamSelector
+ * @extends {window.HTMLElement}
+ */
 export class TeamSelector extends window.HTMLElement {
   constructor () {
     super()
 
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+
     this._input = this.shadowRoot.querySelector('#team-selector')
     this._dataList = this.shadowRoot.querySelector('#teams')
+    this._url = 'http://localhost:3000/api/'
+    this._minLength = 1
     this.teams = []
   }
 
   static get observedAttributes () {
-    return []
+    return ['src', 'minlength']
   }
 
-  attributeChangeCallback (name, olfValue, newValue) {
-
+  attributeChangedCallback (name, olfValue, newValue) {
+    if (name === 'src') {
+      this._url = newValue
+    } else if (name === 'minlength') {
+      this._minLength = parseInt(newValue)
+    }
   }
 
   connectedCallback () {
     this._input.addEventListener('input', async event => {
+      if (this._input.value.length < this._minLength) { return }
       this.teams = await this.search(this._input.value)
       this._updateRendering(this.teams)
+
+      const match = this.teams.filter(team => team.name === this._input.value).shift()
+      if (match) {
+        this.dispatchEvent(new window.CustomEvent('teamselected', { detail: match }))
+        this._input.blur()
+        this._input.focus()
+      }
     })
   }
 
@@ -39,10 +60,9 @@ export class TeamSelector extends window.HTMLElement {
    * @param {string} string
    * @returns {array}
    */
-  async search (string) {
-    let searchResult = await window.fetch(`http://localhost:3000/api/teams?q=${string}`)
+  async search (input) {
+    let searchResult = await window.fetch(`${this._url}/teams?q=${input}`)
     searchResult = await searchResult.json()
-    console.log(searchResult.teams)
     return searchResult.teams
   }
 
@@ -52,12 +72,19 @@ export class TeamSelector extends window.HTMLElement {
    * @param {array} teams
    */
   _updateRendering (teams) {
+    this._dataList.innerHTML = ''
     for (const team of teams) {
-      const opt = document.createElement('option')
-      opt.setAttribute('value', `${team.name}`)
-      this._dataList.appendChild(opt)
+      const _option = document.createElement('option')
+      _option.setAttribute('value', `${team.name}`)
+      this._dataList.appendChild(_option)
     }
   }
 }
 
 window.customElements.define('x-team-selector', TeamSelector)
+
+/**
+ * TODOs:
+ * Add error handling
+ * https://javascript.info/async-await#error-handling
+ */
